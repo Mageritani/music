eefe8ce7d0c60a1505ce49cf2571d718e6cb536b
黃誼康 <r45371@gmaill.com>
2025-03-11 13:13:48 +0800

    offline download

--- app/src/main/java/com/example/music/MainActivity.kt
+++ app/src/main/java/com/example/music/MainActivity.kt
@@ -8,13 +8,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var title : TextView
     private lateinit var search : ImageButton
     private lateinit var notice : ImageButton
     private lateinit var recycler : RecyclerView
+
+    private lateinit var adapter : OfflineTrackAdapter
+    private lateinit var downloadManger: DownloadManger
+    private var offlineTracks = listOf<OfflineTrack>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,10 +34,27 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        downloadManger = DownloadManger(this)
+
         title = findViewById(R.id.title)
         search = findViewById(R.id.search)
         notice = findViewById(R.id.notice)
         recycler = findViewById(R.id.recycler)
+
+        adapter = OfflineTrackAdapter(
+            track = emptyList(),
+            onTrackClick = {track,position ->
+                plaOfflineTrack(track,position)
+            },
+            onTrackDelete = {track ->
+                deleteOfflineTrack(track)
+            }
+        )
+
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.adapter = adapter
+
+        loadOfflineTracks()
 
         search.setOnClickListener {
             val intent = Intent(this,SearchActivity::class.java)
@@ -38,7 +64,48 @@ class MainActivity : AppCompatActivity() {
         notice.setOnClickListener {
             val intent = Intent(this,NoticeActivity::class.java)
             startActivity(intent)
-
         }
+    }
+
+    private fun plaOfflineTrack(track: OfflineTrack, position: Int) {
+        val trackNameList = offlineTracks.map { it.name } as ArrayList<String>
+        val trackImageList = offlineTracks.map{it.image} as ArrayList<String>
+        val trackAudioList = offlineTracks.map { it.localPath } as ArrayList<String>
+
+        val intent = Intent(this,MusicPlayActivity::class.java).apply {
+            putExtra("TRACK_LIST",trackAudioList)
+            putExtra("TRACK_INDEX",position)
+            putExtra("TRACK_NAME",trackNameList)
+            putExtra("TRACK_IMAGE",trackImageList)
+        }
+        startActivity(intent)
+
+    }
+
+    private fun deleteOfflineTrack(track: OfflineTrack) {
+        lifecycleScope.launch {
+            val success = withContext(Dispatchers.IO){
+                downloadManger.deleteOfflineTrack(track)
+            }
+            if (success){
+                loadOfflineTracks()
+            }
+        }
+    }
+
+    private fun loadOfflineTracks() {
+        lifecycleScope.launch {
+            val tracks  = withContext(Dispatchers.IO){
+                downloadManger.getAllOfflineTracks()
+            }
+            offlineTracks = tracks
+            adapter.updateTracks(tracks)
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        loadOfflineTracks()
     }
 }
